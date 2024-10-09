b006e6324ce5a73ad4dae02b25d03c199175aa7c
sj98h <sj98h@naver.com>
2024-10-09 21:51:12 +0900

    fix: 쿠키 토큰을 통한 소켓 연결

--- src/handlers/helper.js
+++ src/handlers/helper.js
@@ -1,14 +1,14 @@
 import handlerMappings from './handlerMapping.js';
 
-export const handleDisconnect = (socket, uuid) => {
+export const handleDisconnect = (socket, userId) => {
   console.log(`User disconnected: ${socket.id}`);
 };
 
-export const handleConnection = (socket, uuid) => {
-  console.log(`New user connected!! ${uuid} with Socket ID: ${socket.id}`);
+export const handleConnection = (socket, userId) => {
+  console.log(`New user connected!! ${userId} with Socket ID: ${socket.id}`);
 
   // 응답
-  socket.emit('connection', { userId: 555 });
+  socket.emit('connection', { userId });
 };
 
 export const handlerEvent = async (io, socket, data) => {
