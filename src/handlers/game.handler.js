c84c0faa05750b33b802c8a810f213d9c8d306e0
yth0417 <yth0417@gmail.com>
2024-10-09 23:32:30 +0900

    add: gameOver 이벤트 추가
    수정된 js
    game / game.handler

--- src/handlers/game.handler.js
+++ src/handlers/game.handler.js
@@ -1,7 +1,23 @@
+import { clearMonsters, getMonsters } from '../models/monster.model.js';
+// import { setScore, getScore } from '../models/score.model.js';
+
 export const gameStart = async (uuid, payload) => {
   //
 };
 
-export const gameEnd = async (uuid, payload) => {
-  //
+export const gameEnd = async (userId, payload) => {
+  const userMonsters = getMonsters(userId);
+
+  if (!userMonsters) {
+    return { status: 'fail', message: 'Monsters not found' };
+  }
+
+  let verificationScore = 0;
+  for (let i = 0; i < userMonsters.length; i++) {
+    verificationScore += (userMonsters[i].level + 1) * 100;
+  }
+  if (verificationScore !== payload.score) {
+    return { status: 'fail', message: 'Score verification failed' };
+  }
+  return { status: 'success', message: 'Game Over' };
 };
