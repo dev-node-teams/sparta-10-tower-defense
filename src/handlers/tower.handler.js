4b91485d7940c9c6670d3ad48ef42f72ea65a3fa
P-lani <dndtkd2@naver.com>
2024-10-10 12:12:10 +0900

    add, towers 에 towerType 추가

--- src/handlers/tower.handler.js
+++ src/handlers/tower.handler.js
@@ -2,8 +2,10 @@ import { setTower, getTower } from '../models/tower.model.js';
 import { findTowers } from '../repositories/towers.repository.js';
 
 export const towerBuy = async (userId, payload) => {
-  setTower(userId, payload.position);
-  console.log('타워찾기', await findTowers());
+  const towerMetadata = await findTowers();
+
+  setTower(userId, payload.towerType, payload.position);
+
   console.log(getTower(userId));
   return { status: 'success', message: ' 타워를 구매했습니다. ', towerbuy: true };
   //
--- src/models/tower.model.js
+++ src/models/tower.model.js
@@ -9,8 +9,8 @@ export const getTower = (userId) => {
   return towers[userId];
 };
 
-export const setTower = (userId, position) => {
-  return towers[userId].push({ position });
+export const setTower = (userId, towerType, position) => {
+  return towers[userId].push({ towerType, position });
 };
 
 export const clearTower = (userId) => {
