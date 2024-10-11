1d5b8348f654cdc0d8918ff39142a55e0b02beb1
P-lani <dndtkd2@naver.com>
2024-10-11 20:18:12 +0900

    ♻️ refactor, tower - redis 적용

--- src/handlers/helper.js
+++ src/handlers/helper.js
@@ -1,4 +1,4 @@
-import { createTower, getTower } from '../models/tower.model.js';
+import { clearTower } from '../models/tower.model.js';
 import { createGold } from '../models/gold.model.js';
 import { createScore } from '../models/score.model.js';
 import { clearStage, createStage } from '../models/stage.model.js';
@@ -13,7 +13,7 @@ export const handleDisconnect = (socket, userId) => {
 export const handleConnection = (socket, userId) => {
   console.log(`New user connected!! ${userId} with Socket ID: ${socket.id}`);
 
-  createTower(userId);
+  clearTower(userId);
 
   createStage(userId);
   // 1 스테이지 생성
--- src/handlers/tower.handler.js
+++ src/handlers/tower.handler.js
@@ -1,43 +1,32 @@
-import { setTower, getTower, deleteTower } from '../models/tower.model.js';
-import { findTowers } from '../repositories/towers.repository.js';
+import { setTower, getTower, updateTower } from '../models/tower.model.js';
 import { clearGold, getGold, setGold, getTotalGold } from '../models/gold.model.js';
+import { getTowerDatas } from '../models/mTower.model.js';
 
+// 타워 구매 핸들러
 export const towerBuy = async (userId, payload) => {
-  const towerMetadata = await findTowers();
-
+  const towerMetadata = await getTowerDatas();
   const findTowerId = towerMetadata.find((id) => id.towerId === payload.towerType);
+
   // 소지금 - findTowerId.price 해서 쌓아두기
   setGold(userId, -findTowerId.price);
-  // 현재 소지금
-  const userTotalGold = getTotalGold(userId);
-
-  console.log('타워 구매 후 잔액 => ', userTotalGold);
 
   // 토탈골드가 마이너스인 경우
-  if (userTotalGold < 0) {
+  if (getTotalGold(userId) < 0) {
     return { status: 'fail', message: ' 치트 검거 완료 ' };
   }
 
-  setTower(userId, payload.towerType, payload.position);
+  await setTower(userId, payload.towerType, payload.position);
+  console.log('겟 타워 확인', await getTower(userId));
 
-  const myTowers = getTower(userId);
-  console.log('타워 구매 후 보유한 타워들', myTowers);
-  const towerCount = myTowers.length;
-
-  return {
-    status: 'success',
-    message: `${findTowerId.name} 타워를 구매했습니다. `,
-    handlerId: 5,
-    userGold: userTotalGold,
-    position: payload.position,
-    remaining: towerCount,
-  };
+  return { status: 'success', message: `${findTowerId.name} 타워를 구매했습니다. ` };
   //
 };
 
+// 타워 판매 핸들러
 export const towerSell = async (userId, payload) => {
-  const towerMetadata = await findTowers();
+  const towerMetadata = await getTowerDatas();
   const findTowerId = towerMetadata.find((id) => id.towerId === payload.towerType);
+
   //판매금액
   const sellPrice = findTowerId.price / 2;
   // 소지금 + findTowerId.price 해서 쌓아두기
@@ -45,16 +34,16 @@ export const towerSell = async (userId, payload) => {
 
   const userTotalGold = getTotalGold(userId);
   console.log('타워 판매 후 잔액 => ', userTotalGold);
+  const beforeTowers = await getTower(userId);
 
-  // deleteTower 가 필요하다..
-  // getTower에서 해당 타워 찾기
-  const myTowers = getTower(userId);
-  const findSellTower = myTowers.find((i) => i.x === payload.x && i.y === payload.y);
-  console.log('판매 대상 타워', findSellTower);
-  deleteTower(userId, findSellTower);
+  //판매 대상
+  const tarGetTower = beforeTowers.find((i) => i.x === payload.x && i.y === payload.y);
 
-  const afterMyTowers = getTower(userId);
-  console.log('타워 판매 후 보유한 타워들', afterMyTowers);
+  //팔고 남은 타워
+  const remainingTowers = beforeTowers.filter((i) => i !== tarGetTower);
+
+  await updateTower(userId, remainingTowers);
+
   const towerCount = afterMyTowers.length;
 
   return {
