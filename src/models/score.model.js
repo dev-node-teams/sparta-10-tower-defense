eb0d13761d3b506ce93eb7a6b0ab5feb10588bb5
sj98h <sj98h@naver.com>
2024-10-11 22:53:23 +0900

    del: 불필요한 주석 삭제

--- src/models/score.model.js
+++ src/models/score.model.js
@@ -1,30 +1,3 @@
-// const score = {};
-
-// const createScore = (userId) => {
-//   score[userId] = [];
-// };
-
-// const getScore = (userId) => {
-//   return score[userId];
-// };
-
-// const setScore = (userId, data) => {
-//   // 매개변수 이름을 임의로 정한 상태 입니다.
-//   // 적절한 이름이 생각나면 변경하겠습니다.
-
-//   score[userId].push(data);
-// };
-
-// const getTotalScore = (userId) => {
-//   return score[userId].reduce((acc, cur) => {
-//     return acc + cur;
-//   });
-// };
-
-// const clearScore = (userId) => {
-//   return (score[userId] = []);
-// };
-
 import redisClient from '../init/redis.js';
 
 const KEY_PREFIX = 'scoredatas:';
